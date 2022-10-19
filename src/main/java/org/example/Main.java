0aaf83dd7382a58473979fd7ecf4f7103db438c7
ElenaRei <113910361+ElenaRei@users.noreply.github.com>
2022-10-19 16:41:00 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,34 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Is it weekend today?");
+
+
+        boolean isWeekend = scanner.nextBoolean();
+
+        System.out.println("Is it rainy today?");
+
+
+        boolean isRain = scanner.nextBoolean();
+
+
+        if (isWeekend == true && isRain == false) {
+
+            boolean canWalk = true;
+
+            System.out.println("canWalk = " + canWalk);
+
+        } else {
+
+            boolean canWalk = false;
+
+            System.out.println("canWalk= " + canWalk);
+
+        }
     }
 }
